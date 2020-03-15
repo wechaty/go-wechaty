24dc14c1b166f7aaacc44c6029ed0128fb111d13
dingdayu <614422099@qq.com>
2020-03-15 21:25:13 +0800

    add wechaty/user/images.go
    Signed-off-by: dingdayu <614422099@qq.com>

---
+++ wechaty/user/image.go
@@ -0,0 +1,35 @@
+package user
+
+import (
+	"github.com/wechaty/go-wechaty/wechaty"
+	wechatyPuppet "github.com/wechaty/go-wechaty/wechaty-puppet"
+	"github.com/wechaty/go-wechaty/wechaty-puppet/schemas"
+)
+
+type Images struct {
+	wechaty.Accessory
+	ImageId string
+}
+
+// NewImages create image struct
+func NewImages(id string, accessory wechaty.Accessory) *Images {
+	if accessory.Puppet == nil {
+		panic("Image class can not be instanciated without a puppet!")
+	}
+	return &Images{accessory, id}
+}
+
+// Thumbnail message thumbnail images
+func (img *Images) Thumbnail() wechatyPuppet.FileBox {
+	return img.Accessory.Puppet.MessageImage(img.ImageId, schemas.Thumbnail)
+}
+
+// HD message hd images
+func (img *Images) HD() wechatyPuppet.FileBox {
+	return img.Accessory.Puppet.MessageImage(img.ImageId, schemas.HD)
+}
+
+// Artwork message artwork images
+func (img *Images) Artwork() wechatyPuppet.FileBox {
+	return img.Accessory.Puppet.MessageImage(img.ImageId, schemas.Artwork)
+}
