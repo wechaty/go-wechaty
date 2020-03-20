64d47e0b492726f21d948eb1449398d972c0100b
dingdayu <614422099@qq.com>
2020-03-20 17:42:00 +0800

    update Accessory struct to Accessory interface
    Signed-off-by: dingdayu <614422099@qq.com>

--- src/wechaty/user/image.go
+++ src/wechaty/user/image.go
@@ -13,7 +13,7 @@ type Images struct {
 
 // NewImages create image struct
 func NewImages(id string, accessory wechaty.Accessory) *Images {
-	if accessory.Puppet == nil {
+	if accessory.GetPuppet() == nil {
 		panic("Image class can not be instanciated without a puppet!")
 	}
 	return &Images{accessory, id}
@@ -21,15 +21,15 @@ func NewImages(id string, accessory wechaty.Accessory) *Images {
 
 // Thumbnail message thumbnail images
 func (img *Images) Thumbnail() wechatyPuppet.FileBox {
-	return img.Accessory.Puppet.MessageImage(img.ImageId, schemas.ImageTypeThumbnail)
+	return img.Accessory.GetPuppet().MessageImage(img.ImageId, schemas.ImageTypeThumbnail)
 }
 
 // HD message hd images
 func (img *Images) HD() wechatyPuppet.FileBox {
-	return img.Accessory.Puppet.MessageImage(img.ImageId, schemas.ImageTypeHD)
+	return img.Accessory.GetPuppet().MessageImage(img.ImageId, schemas.ImageTypeHD)
 }
 
 // Artwork message artwork images
 func (img *Images) Artwork() wechatyPuppet.FileBox {
-	return img.Accessory.Puppet.MessageImage(img.ImageId, schemas.ImageTypeArtwork)
+	return img.Accessory.GetPuppet().MessageImage(img.ImageId, schemas.ImageTypeArtwork)
 }
