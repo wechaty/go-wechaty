b5f29208c9f116f0b92ce4c8c07b140c515de8c9
Silkage <silkage@silkage.net>
2020-04-03 14:26:51 +0800

    add puppet schemas

---
+++ src/wechaty-puppet/schemas/mini_program.go
@@ -0,0 +1,11 @@
+package schemas
+
+type MiniProgramPayload struct {
+  AppId       string
+  Description string
+  PagePath    string
+  ThumbUrl    string
+  Title       string
+  Username    string
+  ThumbKey    string
+}
