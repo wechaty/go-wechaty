9fa893477abdd704f8d801c1913cba677ac34552
dingdayu <614422099@qq.com>
2020-04-24 13:17:22 +0800

    go fmt examples/ding-dong-bot.go

--- examples/ding-dong-bot.go
+++ examples/ding-dong-bot.go
@@ -1,42 +1,43 @@
 package main
 
 import (
-  "fmt"
-  "github.com/wechaty/go-wechaty/wechaty"
-  "github.com/wechaty/go-wechaty/wechaty-puppet/option"
-  "github.com/wechaty/go-wechaty/wechaty-puppet/schemas"
-  "github.com/wechaty/go-wechaty/wechaty/user"
-  "log"
-  "os"
-  "os/signal"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/wechaty/go-wechaty/wechaty"
+	"github.com/wechaty/go-wechaty/wechaty-puppet/option"
+	"github.com/wechaty/go-wechaty/wechaty-puppet/schemas"
+	"github.com/wechaty/go-wechaty/wechaty/user"
 )
 
 func main() {
-  var bot = wechaty.NewWechaty(wechaty.WithPuppetOption(&option.Option{
-    Token:        "",
-  }))
+	var bot = wechaty.NewWechaty(wechaty.WithPuppetOption(&option.Option{
+		Token: "",
+	}))
 
-  bot.
-    OnScan(func(qrCode string, status schemas.ScanStatus, data string) {
-      fmt.Printf("Scan QR Code to login: %v\nhttps://api.qrserver.com/v1/create-qr-code/?data=%s\n", status, qrCode)
-    }).
-    OnLogin(func(user string) {
-      fmt.Printf("User %s logined\n", user)
-    }).
-    OnMessage(func(message *user.Message) {
-      fmt.Println(fmt.Printf("Message: %v\n", message))
-    })
+	bot.
+		OnScan(func(qrCode string, status schemas.ScanStatus, data string) {
+			fmt.Printf("Scan QR Code to login: %v\nhttps://api.qrserver.com/v1/create-qr-code/?data=%s\n", status, qrCode)
+		}).
+		OnLogin(func(user string) {
+			fmt.Printf("User %s logined\n", user)
+		}).
+		OnMessage(func(message *user.Message) {
+			fmt.Println(fmt.Printf("Message: %v\n", message))
+		})
 
-  var err = bot.Start()
-  if err != nil {
-    panic(err)
-  }
+	var err = bot.Start()
+	if err != nil {
+		panic(err)
+	}
 
-  var quitSig = make(chan os.Signal)
-  signal.Notify(quitSig, os.Interrupt, os.Kill)
+	var quitSig = make(chan os.Signal)
+	signal.Notify(quitSig, os.Interrupt, os.Kill)
 
-  select {
-  case <-quitSig:
-    log.Fatal("exit.by.signal")
-  }
+	select {
+	case <-quitSig:
+		log.Fatal("exit.by.signal")
+	}
 }
