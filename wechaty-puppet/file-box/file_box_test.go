1701add124b6bd7f48bef780be9cb9176bb0f8de
Chao Fei (Rebase PR Action) <dchaofei@163.com>
2020-05-06 16:41:28 +0000

    Formatting files

--- wechaty-puppet/file-box/file_box_test.go
+++ wechaty-puppet/file-box/file_box_test.go
@@ -1,92 +1,92 @@
 package file_box
 
 import (
-  "encoding/base64"
-  "io/ioutil"
-  "log"
-  "os"
-  "reflect"
-  "testing"
+	"encoding/base64"
+	"io/ioutil"
+	"log"
+	"os"
+	"reflect"
+	"testing"
 )
 
 func TestFileBox_ToFile(t *testing.T) {
-  expect := "test content"
-  fileBox := NewFileBoxFromJSONObjectBase64(&FileBoxJsonObjectBase64{
-    FileBoxJsonObjectCommon: &FileBoxJsonObjectCommon{
-      Name:     "test.text",
-      Metadata: nil,
-      BoxType:  FileBoxTypeBase64,
-    },
-    Base64: base64.StdEncoding.EncodeToString([]byte(expect)),
-  })
-  const filename = "testdata/test.text"
-  t.Run("toFile success", func(t *testing.T) {
-    err := fileBox.ToFile(filename, true)
-    if err != nil {
-      log.Fatal(err)
-    }
-    file, err := os.Open(filename)
-    if err != nil {
-      log.Fatal(err)
-    }
-    got, err := ioutil.ReadAll(file)
-    if err != nil {
-      log.Fatal(err)
-    }
-    if expect != string(got) {
-      log.Fatalf("got %s expect %s", got, expect)
-    }
-  })
-  t.Run("file exists", func(t *testing.T) {
-    err := fileBox.ToFile(filename, false)
-    if err != os.ErrExist {
-      log.Fatalf("got %s expect %s", err, os.ErrExist)
-    }
-  })
+	expect := "test content"
+	fileBox := NewFileBoxFromJSONObjectBase64(&FileBoxJsonObjectBase64{
+		FileBoxJsonObjectCommon: &FileBoxJsonObjectCommon{
+			Name:     "test.text",
+			Metadata: nil,
+			BoxType:  FileBoxTypeBase64,
+		},
+		Base64: base64.StdEncoding.EncodeToString([]byte(expect)),
+	})
+	const filename = "testdata/test.text"
+	t.Run("toFile success", func(t *testing.T) {
+		err := fileBox.ToFile(filename, true)
+		if err != nil {
+			log.Fatal(err)
+		}
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		got, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if expect != string(got) {
+			log.Fatalf("got %s expect %s", got, expect)
+		}
+	})
+	t.Run("file exists", func(t *testing.T) {
+		err := fileBox.ToFile(filename, false)
+		if err != os.ErrExist {
+			log.Fatalf("got %s expect %s", err, os.ErrExist)
+		}
+	})
 }
 
 func TestNewFileBoxFromJSONString(t *testing.T) {
-  tests := []struct {
-    jsonString     string
-    expectFileImpl reflect.Type
-  }{
-    {
-      jsonString: `{
+	tests := []struct {
+		jsonString     string
+		expectFileImpl reflect.Type
+	}{
+		{
+			jsonString: `{
 "Name":"test.png",
 "metadata": null,
 "boxType":1,
 "base64":"dGVzdCBjb250ZW50"
 }`,
-      expectFileImpl: reflect.TypeOf(new(fileBoxBase64)),
-    },
-    {
-      jsonString: `{
+			expectFileImpl: reflect.TypeOf(new(fileBoxBase64)),
+		},
+		{
+			jsonString: `{
 "Name":"test.png",
 "metadata": null,
 "boxType":2,
 "remoteUrl":"http://www.example.com",
 "header":null
 }`,
-      expectFileImpl: reflect.TypeOf(new(fileBoxUrl)),
-    },
-    {
-      jsonString: `{
+			expectFileImpl: reflect.TypeOf(new(fileBoxUrl)),
+		},
+		{
+			jsonString: `{
 "Name":"test.png",
 "metadata": null,
 "boxType":3,
 "qrCode":"test content"
 }`,
-      expectFileImpl: reflect.TypeOf(new(fileBoxQRCode)),
-    },
-  }
-  for _, t := range tests {
-    fileBox, err := FromJSON(t.jsonString)
-    if err != nil {
-      log.Fatal(err)
-    }
-    gotReflectType := reflect.TypeOf(fileBox.fileImpl)
-    if gotReflectType != t.expectFileImpl {
-      log.Fatalf("got %v expect %v", gotReflectType, t.expectFileImpl)
-    }
-  }
+			expectFileImpl: reflect.TypeOf(new(fileBoxQRCode)),
+		},
+	}
+	for _, t := range tests {
+		fileBox, err := FromJSON(t.jsonString)
+		if err != nil {
+			log.Fatal(err)
+		}
+		gotReflectType := reflect.TypeOf(fileBox.fileImpl)
+		if gotReflectType != t.expectFileImpl {
+			log.Fatalf("got %v expect %v", gotReflectType, t.expectFileImpl)
+		}
+	}
 }
--- wechaty/interface/tag.go
+++ wechaty/interface/tag.go
@@ -1,7 +1,7 @@
 package _interface
 
 type ITagFactory interface {
-    Load(id string) ITag
+	Load(id string) ITag
 }
 
 type ITag interface {
