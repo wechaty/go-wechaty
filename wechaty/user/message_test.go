6f6dbbd39f9d80fb69ba4e8a3ca78a03330c2bfa
Chao Fei (Rebase PR Action) <dchaofei@163.com>
2020-05-15 05:08:23 +0000

    Formatting files

--- wechaty/user/message_test.go
+++ wechaty/user/message_test.go
@@ -1,12 +1,12 @@
 package user
 
 import (
-    "fmt"
-    "testing"
+	"fmt"
+	"testing"
 )
 
 func TestMessage_MentionList(t *testing.T) {
-    u := NewMessage("", nil).(*Message)
-    at := u.multipleAt("hello@a@b@c")
-    fmt.Println(at)
+	u := NewMessage("", nil).(*Message)
+	at := u.multipleAt("hello@a@b@c")
+	fmt.Println(at)
 }
