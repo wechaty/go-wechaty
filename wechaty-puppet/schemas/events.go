330b93884ad94ee79b4f8c4a361c6447b84e8036
Chao Fei (Rebase PR Action) <dchaofei@163.com>
2020-05-27 02:07:43 +0000

    Formatting files

--- wechaty-puppet/schemas/events.go
+++ wechaty-puppet/schemas/events.go
@@ -4,65 +4,65 @@ package schemas
 type ScanStatus uint8
 
 const (
-  ScanStatusUnknown   ScanStatus = 0
-  ScanStatusCancel    ScanStatus = 1
-  ScanStatusWaiting   ScanStatus = 2
-  ScanStatusScanned   ScanStatus = 3
-  ScanStatusConfirmed ScanStatus = 4
-  ScanStatusTimeout   ScanStatus = 5
+	ScanStatusUnknown   ScanStatus = 0
+	ScanStatusCancel    ScanStatus = 1
+	ScanStatusWaiting   ScanStatus = 2
+	ScanStatusScanned   ScanStatus = 3
+	ScanStatusConfirmed ScanStatus = 4
+	ScanStatusTimeout   ScanStatus = 5
 )
 
 type EventFriendshipPayload struct {
-  FriendshipID string
+	FriendshipID string
 }
 
 type EventLoginPayload struct {
-  ContactId string
+	ContactId string
 }
 
 type EventLogoutPayload struct {
-  ContactId string
-  Data      string
+	ContactId string
+	Data      string
 }
 
 type EventMessagePayload struct {
-  MessageId string
+	MessageId string
 }
 
 type EventRoomInvitePayload struct {
-  RoomInvitationId string
+	RoomInvitationId string
 }
 
 type EventRoomJoinPayload struct {
-  InviteeIdList []string
-  InviterId     string
-  RoomId        string
-  Timestamp     int64
+	InviteeIdList []string
+	InviterId     string
+	RoomId        string
+	Timestamp     int64
 }
 
 type EventRoomLeavePayload struct {
-  RemoveeIdList []string
-  RemoverId     string
-  RoomId       string
-  Timestamp    int64
+	RemoveeIdList []string
+	RemoverId     string
+	RoomId        string
+	Timestamp     int64
 }
 
 type EventRoomTopicPayload struct {
-  ChangerId string
-  NewTopic  string
-  OldTopic  string
-  RoomId    string
-  Timestamp int64
+	ChangerId string
+	NewTopic  string
+	OldTopic  string
+	RoomId    string
+	Timestamp int64
 }
 
 type EventScanPayload struct {
-  BaseEventPayload
-  Status ScanStatus
-  QrCode string
+	BaseEventPayload
+	Status ScanStatus
+	QrCode string
 }
 
 type BaseEventPayload struct {
-  Data string
+	Data string
 }
 
 type EventDongPayload = BaseEventPayload
