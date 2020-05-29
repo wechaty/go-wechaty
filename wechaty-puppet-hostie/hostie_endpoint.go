5d393a37499815c04c641c04b0c439f257962459
dchaofei <dchaofei@163.com>
2020-05-29 10:02:40 +0800

    fix endpoint isValid

--- wechaty-puppet-hostie/hostie_endpoint.go
+++ wechaty-puppet-hostie/hostie_endpoint.go
@@ -21,7 +21,7 @@ type HostieEndPoint struct {
 
 // IsValid EndPoint is valid
 func (p *HostieEndPoint) IsValid() bool {
-	return p.IP == "" || p.IP == "0.0.0.0"
+	return p.IP != "" && p.IP != "0.0.0.0"
 }
 
 // Target Export IP+Port
