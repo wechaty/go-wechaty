6350b302f03fc47283b686d8e40406a9288646e5
FINCTIVE <finctive@qq.com>
2020-09-05 15:00:00 +0800

    event changed

--- wechaty/event.go
+++ wechaty/event.go
@@ -9,48 +9,33 @@ import (
 
 type (
 	// EventDong ...
-	EventDong       func(data string)
-	PluginEventDong func(context *PluginContext, data string)
+	EventDong func(context *PluginContext, data string)
 	// EventError ...
-	EventError       func(err error)
-	PluginEventError func(context *PluginContext, err error)
+	EventError func(context *PluginContext, err error)
 	// EventFriendship ...
-	EventFriendship       func(friendship *user.Friendship)
-	PluginEventFriendship func(context *PluginContext, friendship *user.Friendship)
+	EventFriendship func(context *PluginContext, friendship *user.Friendship)
 	// EventHeartbeat ...
-	EventHeartbeat       func(data string)
-	PluginEventHeartbeat func(context *PluginContext, data string)
+	EventHeartbeat func(context *PluginContext, data string)
 	// EventLogin ...
-	EventLogin       func(user *user.ContactSelf)
-	PluginEventLogin func(context *PluginContext, user *user.ContactSelf)
+	EventLogin func(context *PluginContext, user *user.ContactSelf)
 	// EventLogout ...
-	EventLogout       func(user *user.ContactSelf, reason string)
-	PluginEventLogout func(context *PluginContext, user *user.ContactSelf, reason string)
+	EventLogout func(context *PluginContext, user *user.ContactSelf, reason string)
 	// EventMessage ...
-	EventMessage       func(message *user.Message)
-	PluginEventMessage func(context *PluginContext, message *user.Message)
+	EventMessage func(context *PluginContext, message *user.Message)
 	// EventReady ...
-	EventReady       func()
-	PluginEventReady func(context *PluginContext)
+	EventReady func(context *PluginContext)
 	// EventRoomInvite ...
-	EventRoomInvite       func(roomInvitation *user.RoomInvitation)
-	PluginEventRoomInvite func(context *PluginContext, roomInvitation *user.RoomInvitation)
+	EventRoomInvite func(context *PluginContext, roomInvitation *user.RoomInvitation)
 	// EventRoomJoin ...
-	EventRoomJoin       func(room *user.Room, inviteeList []_interface.IContact, inviter _interface.IContact, date time.Time)
-	PluginEventRoomJoin func(context *PluginContext, room *user.Room, inviteeList []_interface.IContact, inviter _interface.IContact, date time.Time)
+	EventRoomJoin func(context *PluginContext, room *user.Room, inviteeList []_interface.IContact, inviter _interface.IContact, date time.Time)
 	// EventRoomLeave ...
-	EventRoomLeave       func(room *user.Room, leaverList []_interface.IContact, remover _interface.IContact, date time.Time)
-	PluginEventRoomLeave func(context *PluginContext, room *user.Room, leaverList []_interface.IContact, remover _interface.IContact, date time.Time)
+	EventRoomLeave func(context *PluginContext, room *user.Room, leaverList []_interface.IContact, remover _interface.IContact, date time.Time)
 	// EventRoomTopic ...
-	EventRoomTopic       func(room *user.Room, newTopic string, oldTopic string, changer _interface.IContact, date time.Time)
-	PluginEventRoomTopic func(context *PluginContext, room *user.Room, newTopic string, oldTopic string, changer _interface.IContact, date time.Time)
+	EventRoomTopic func(context *PluginContext, room *user.Room, newTopic string, oldTopic string, changer _interface.IContact, date time.Time)
 	// EventScan ...
-	EventScan       func(qrCode string, status schemas.ScanStatus, data string)
-	PluginEventScan func(context *PluginContext, qrCode string, status schemas.ScanStatus, data string)
+	EventScan func(context *PluginContext, qrCode string, status schemas.ScanStatus, data string)
 	// EventStart ...
-	EventStart       func()
-	PluginEventStart func(context *PluginContext)
+	EventStart func(context *PluginContext)
 	// EventStop ...
-	EventStop       func()
-	PluginEventStop func(context *PluginContext)
+	EventStop func(context *PluginContext)
 )
