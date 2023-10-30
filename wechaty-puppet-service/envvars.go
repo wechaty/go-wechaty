01b24f20b08d984caf9967a649239b36685fbe54
dchaofei <dchaofei@163.com>
2023-10-30 14:11:26 +0800

    fix: compatible WECHATY_PUPPET_HOSTIE_TOKEN

--- wechaty-puppet-service/envvars.go
+++ wechaty-puppet-service/envvars.go
@@ -18,7 +18,7 @@ func envServiceToken(token string) (string, error) {
 		return token, nil
 	}
 
-	token = os.Getenv("WECHATY_PUPPET_HOSTIE_ENDPOINT")
+	token = os.Getenv("WECHATY_PUPPET_HOSTIE_TOKEN")
 	if token != "" {
 		log.Trace("WECHATY_PUPPET_HOSTIE_TOKEN has been deprecated," +
 			"please use WECHATY_PUPPET_SERVICE_TOKEN instead.")
